eaeeb3f65c76fe930832633333df8b26ea967e18
Mlacic Vinko <vinko.mlacic@etudiant.univ-rennes1.fr>
2020-03-14 16:49:54 +0100

    Adjust pstate_t struct order and size for alignment in x64 machines

--- include/pstate_t.h
+++ include/pstate_t.h
@@ -2,22 +2,22 @@
 
 #include <sys/user.h>
 #include <stdbool.h>
-#include "unistd.h"
-#include "stdint.h"
+#include <unistd.h>
+#include <stdint.h>
 
 #define PROCESS_NAME_MAX 256
-#define CODE_TO_RESTORE_MAX 32
+#define CODE_TO_RESTORE_MAX 34
 
 /**
  * Definition of a struct which holds process state.
  */
 typedef struct PSTATE {
-    pid_t pid;
-    char name[PROCESS_NAME_MAX];
-    intptr_t change_address;
-    unsigned short changed_code_len;
-    uint8_t changed_code[CODE_TO_RESTORE_MAX];
     struct user_regs_struct changed_regs;
+    intptr_t change_address;
+    pid_t pid;
+    unsigned short changed_code_len;
+    char name[PROCESS_NAME_MAX];
+    uint8_t changed_code[CODE_TO_RESTORE_MAX];
 } pstate_t;
 
 
